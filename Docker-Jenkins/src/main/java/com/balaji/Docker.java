f3fe5262e3de630563e343d0c01461b995f72c60
balaj <balaj@192.168.0.104>
2022-03-14 01:42:36 +0530

    Project 4

---
+++ Docker-Jenkins/src/main/java/com/balaji/Docker.java
@@ -0,0 +1,11 @@
+package com.balaji;
+
+public class Docker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to docker and jenkins pipeline"); 
+
+	}
+
+}
